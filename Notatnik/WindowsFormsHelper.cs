184deb19c44d34332d540ee854d9745010e2f862
Humpty <ainodeve@gmail.com>
2019-04-21 22:29:03 +0200

    Base for fonts

--- Notatnik/WindowsFormsHelper.cs
+++ Notatnik/WindowsFormsHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Media;
+using System.Windows;
 
 namespace NoteUtils
 {
@@ -7,7 +8,12 @@ namespace NoteUtils
         public static System.Drawing.Color Convert(this Color color)
         {
             return System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
-        }
+        } //Convert From Windows.Media
+
+        public static Color Convert(this System.Drawing.Color color)
+        {
+            return Color.FromArgb(color.A, color.R, color.G, color.B);
+        } //Convert from Windows.Drawing
 
         /*public static Color Convert(this System.Drawing.Color color)
         {
@@ -19,11 +25,6 @@ namespace NoteUtils
                 B = color.B
             };
         }*/
-
-        public static Color Convert(this System.Drawing.Color color)
-        {
-            return Color.FromArgb(color.A, color.R, color.G, color.B);
-        }
 
         public static bool ChooseColor(ref Color color)
         {
@@ -39,5 +40,74 @@ namespace NoteUtils
                 return result;
             }
         }
+
+        public struct Font
+        {
+            public FontFamily Family { get; set; }
+
+            public string FamilyName
+            {
+                get
+                {
+                    return Family.ToString();
+                }
+            }
+
+            public FontStyle Style { get; set; }
+            public FontWeight Weight { get; set; }
+            public TextDecorationCollection textDecorations { get; set; }
+            public double Size { get; set; }
+            public Color Color { get; set; }
+            public Brush brush { get { return new SolidColorBrush(Color); } }
+
+            public static Font Default
+            {
+                get
+                {
+                    return new Font()
+                    {
+                        Family = new FontFamily("Segoe UI"),
+                        Style = FontStyles.Normal,
+                        Weight = FontWeights.Normal,
+                        textDecorations = null,
+                        Size = 12,
+                        Color = Colors.Black
+                    };
+                }
+            }
+
+            public static System.Drawing.Font defineToSystemDrawingFont(Font font)
+            {
+                System.Drawing.FontStyle style = (font.Style == FontStyles.Italic) ?
+                    System.Drawing.FontStyle.Italic : System.Drawing.FontStyle.Regular;
+                if (font.Weight == FontWeights.Bold)
+                    style |= System.Drawing.FontStyle.Bold;
+                if (font.textDecorations.Contains(System.Windows.TextDecorations.Underline[0]))
+                    style |= System.Drawing.FontStyle.Underline;
+                if (font.textDecorations.Contains(System.Windows.TextDecorations.Strikethrough[0]))
+                    style |= System.Drawing.FontStyle.Strikeout;
+                System.Drawing.Font _Font = new System.Drawing.Font(font.FamilyName, (int)font.Size, style);
+                return _Font;
+            }
+
+            public static Font defineToWindowsMedia(System.Drawing.Font sdFont, System.Drawing.Color sdColor)
+            {
+                Font font = new Font();
+                font.Family = new FontFamily(sdFont.FontFamily.Name);
+                font.Style = sdFont.Italic ? FontStyles.Italic : FontStyles.Normal;
+                font.Weight = sdFont.Bold ? FontWeights.Bold : FontWeights.Regular;
+                font.textDecorations = new TextDecorationCollection();
+                if (sdFont.Underline)
+                    font.textDecorations.Add(System.Windows.TextDecorations.Underline);
+                if (sdFont.Strikeout)
+                    font.textDecorations.Add(System.Windows.TextDecorations.Strikethrough);
+                font.Size = sdFont.Size;
+                font.Color = sdColor.Convert();
+                return font;
+            }
+        }
     }
 }
+
+
+
